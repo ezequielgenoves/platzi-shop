9aacdb8602d07a83bc2ed4be27f52c5ae1c2f5d1
ezequielgenoves <ezequielgenoves0@gmail.com>
2023-03-21 19:55:48 -0300

    Fix configuration for website

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Layout from '@containers/Layout'
 import Home from '@pages/Home'
 import Login from '@pages/Login'
@@ -19,26 +19,26 @@ const App = () => {
   const initialState = useInitialState()
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter basename='/platzi-shop'> 
+      <HashRouter> 
         <Layout>
           <Routes>
-            <Route exact path='/' Component={Home} />
-            <Route exact path='/login' Component={Login} />
+            <Route exact path='#/' Component={Home} />
+            <Route exact path='#/login' Component={Login} />
             <Route
               exact
               path='/password/recovery'
               Component={PasswordRecovery}
             />
-            <Route exact path='/send-email' Component={SendEmail} />
-            <Route exact path='/password/new' Component={NewPassword} />
-            <Route exact path='/account' Component={MyAccount} />
-            <Route exact path='/signup' Component={CreateAccount} />
-            <Route exact path='/checkout' Component={Checkout} />
-            <Route exact path='/orders' Component={Orders} />
+            <Route exact path='#/send-email' Component={SendEmail} />
+            <Route exact path='#/password/new' Component={NewPassword} />
+            <Route exact path='#/account' Component={MyAccount} />
+            <Route exact path='#/signup' Component={CreateAccount} />
+            <Route exact path='#/checkout' Component={Checkout} />
+            <Route exact path='#/orders' Component={Orders} />
             <Route path='*' Component={NotFound} />
           </Routes>
         </Layout>
-      </BrowserRouter>
+      </HashRouter>
     </AppContext.Provider>
   )
 }
