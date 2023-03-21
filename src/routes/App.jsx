3446e04c10700cc653bc3b3001a2057e3a96bd53
ezequielgenoves <ezequielgenoves0@gmail.com>
2023-03-21 19:47:48 -0300

    minimal change for deploy test

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState()
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter basename='/platzi-shop'>
+      <BrowserRouter basename='/platzi-shop'> 
         <Layout>
           <Routes>
             <Route exact path='/' Component={Home} />
